bfdb794ff07e6ec93637c429a17dad5e5e8d4004
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-05-30 15:11:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const element = document.querySelectorAll('.population');
+const arr = [...element].map((item) => {
+  return Number(item.textContent.replaceAll(',', ''));
+});
+
+const total = arr.reduce((sum, item) => sum + item, 0);
+const average = Math.floor(total / arr.length);
+
+document.querySelector('.total-population').textContent = toFormatted(total);
+
+document.querySelector('.average-population').textContent =
+  toFormatted(average);
+
+function toFormatted(number) {
+  const arr2 = String(number).split('');
+  const resultArr = [];
+  let count = 0;
+
+  for (let i = arr2.length - 1; i >= 0; i--) {
+    resultArr.push(arr2[i]);
+    count++;
+
+    if (count === 3 && i !== 0) {
+      resultArr.push(',');
+      count = 0;
+    }
+  }
+
+  return resultArr.reverse().join('');
+}
+
+// document.querySelector('.total-population').textContent =
+// total.toLocaleString();
+// document.querySelector('.average-population').textContent =
+// average.toLocaleString();
